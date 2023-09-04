cd79e3a3f87f6c3dabfeff31d87832c80c7ba37c
Rajneesh Katkam <rajneesh.katkam@plivo.com>
2023-09-05 01:34:01 +0530

    Added code for hamburger close. Removed preload images

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,21 @@ window.onload = function () {
 };
  
 
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the navbar collapse element
+  const navbarCollapse = document.querySelector(".navbar-collapse");
+
+  // Get all the nav links inside the navbar
+  const navLinks = navbarCollapse.querySelectorAll(".nav-link");
+
+  // Add a click event listener to each nav link
+  navLinks.forEach(function (navLink) {
+      navLink.addEventListener("click", function () {
+          // Check if the navbar collapse element is open
+          if (navbarCollapse.classList.contains("show")) {
+              // Close the navbar collapse
+              navbarCollapse.classList.remove("show");
+          }
+      });
+  });
+});
