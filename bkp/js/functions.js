428c4f5358f92a0e7c57e81b9d8a8559448c02ec
Rajneesh Katkam <rajneesh.katkam@gmail.com>
2023-09-04 17:22:43 +0530

    Starting with boilerplate

---
+++ bkp/js/functions.js
@@ -0,0 +1,40 @@
+function option1(){
+   var x= document.getElementById("description1");
+   if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+
+}
+
+function option2(){
+    var x= document.getElementById("description2");
+    if (x.style.display === "block") {
+     x.style.display = "none";
+   } else {
+     x.style.display = "block";
+   }
+ }
+ 
+ 
+ function option3(){
+    var x= document.getElementById("description3");
+    if (x.style.display === "block") {
+     x.style.display = "none";
+   } else {
+     x.style.display = "block";
+   }
+ }
+
+ function option4(){
+    var x= document.getElementById("description4");
+    if (x.style.display === "block") {
+     x.style.display = "none";
+   } else {
+     x.style.display = "block";
+   }
+ }
+ 
+ 
+
