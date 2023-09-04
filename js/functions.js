bfd8d2ec3285c418ea09793c9251cf9cef3a4f6b
Rajneesh Katkam <rajneesh.katkam@plivo.com>
2023-09-05 01:11:45 +0530

    Added loading page

--- js/functions.js
+++ js/functions.js
@@ -1,40 +1,9 @@
-function option1(){
-   var x= document.getElementById("description1");
-   if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-
-}
-
-function option2(){
-    var x= document.getElementById("description2");
-    if (x.style.display === "block") {
-     x.style.display = "none";
-   } else {
-     x.style.display = "block";
-   }
- }
- 
- 
- function option3(){
-    var x= document.getElementById("description3");
-    if (x.style.display === "block") {
-     x.style.display = "none";
-   } else {
-     x.style.display = "block";
-   }
- }
-
- function option4(){
-    var x= document.getElementById("description4");
-    if (x.style.display === "block") {
-     x.style.display = "none";
-   } else {
-     x.style.display = "block";
-   }
- }
- 
+window.onload = function () {
+  // Hide the loading page
+  document.getElementById("loading-page").style.display = "none";
+  
+  // Display the actual content
+  document.getElementById("content").style.display = "block";
+};
  
 
